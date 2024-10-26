c0c0b900811f522ff1c76aef5bbe5b06d02273cf
albertpnet <albertplbusiness@gmail.com>
2024-10-26 19:54:56 +0200

    base map done, boss missing

---
+++ Wizard_Chronicles/levels/Dungeon32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Dungeon32x32" tilewidth="32" tileheight="32" tilecount="255" columns="15">
+ <image source="../images/dungeonResized32x32.png" width="480" height="566"/>
+</tileset>
