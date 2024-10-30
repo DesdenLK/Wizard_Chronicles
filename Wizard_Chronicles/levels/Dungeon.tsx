d76f656ed033d11d06b9628619341a22e4606e44
DesdenLK <luca.acosta@estudiantat.upc.edu>
2024-10-30 20:59:08 +0100

    cleaned build

--- Wizard_Chronicles/levels/Dungeon.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Dungeon" tilewidth="16" tileheight="16" tilecount="270" columns="15">
- <image source="../images/Dungeon.png" trans="ff00ff" width="240" height="288"/>
-</tileset>
--- Wizard_Chronicles/levels/TIleSet.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="TIleSet" tilewidth="32" tileheight="32" tilecount="540" columns="20">
- <image source="../images/Swamp.png" trans="ff00ff" width="640" height="864"/>
- <tile id="87">
-  <objectgroup draworder="index" id="4">
-   <object id="4" x="0.170553" y="22.513" width="32" height="9.40173"/>
-  </objectgroup>
- </tile>
-</tileset>
